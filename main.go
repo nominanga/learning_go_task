362e1885bf17cccea0d32dfad6b752053359b382
dudukin matvei <matduduk@gmail.com>
2026-04-19 23:32:12 +0300

    added godoc

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
+// Package learning_go_task provides basic math utilities.
 package learning_go_task
 
+// Add functions provides functionality of two ints addition
+// for more information visit
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(val1, val2 int) int {
 	return val1 + val2
 }
