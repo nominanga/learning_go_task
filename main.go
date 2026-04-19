e5750fa52b7153e09ef127890c9feb73d3b3c028
dudukin matvei <matduduk@gmail.com>
2026-04-19 23:49:17 +0300

    old version

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // Package learning_go_task provides basic math utilities.
 package learning_go_task
 
-// Add functions provides functionality of two ints addition
+// Add functions provides functionality of two variables addition
 // for more information visit
 // https://www.mathsisfun.com/numbers/addition.html
 func Add(val1, val2 int) int {
